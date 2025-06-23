27786a35d65696dfb558dd0a172cabfa8c4d1dad
Adam Levi <adamlevi87@gmail.com>
2025-06-23 20:17:52 +0300

    updatd

--- local/lambda-mock/index.js
+++ local/lambda-mock/index.js
@@ -3,6 +3,8 @@ const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
 
 AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
   endpoint: process.env.AWS_ENDPOINT
 });
