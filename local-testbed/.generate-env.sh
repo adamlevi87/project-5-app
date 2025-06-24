0c4ddd9aebac71f4fc3b59ae882da582e7932408
Adam Levi <adamlevi87@gmail.com>
2025-06-24 22:19:01 +0300

    updated generate env

--- local-testbed/.generate-env.sh
+++ local-testbed/.generate-env.sh
@@ -7,12 +7,12 @@ set +a
 # Compose combined variables
 echo "Generating .env.generated..."
 
-cat > .env.generated <<EOF
+cat > .env <<EOF
 # Composed variables
 REACT_APP_BACKEND_URL=http://${BACKEND_HOST}:${BACKEND_PORT}
-SQS_QUEUE_URL=http://localstack:${LOCALSTACK_PORT}/000000000000/${QUEUE_NAME}
+SQS_QUEUE_URL=http://${LOCALSTACK_HOST}:${LOCALSTACK_PORT}/000000000000/${QUEUE_NAME}
 AWS_ENDPOINT=http://${LOCALSTACK_HOST}:${LOCALSTACK_PORT}
 EOF
 
 # Append all base vars
-cat .env >> .env.generated
+cat .env.base >> .env
